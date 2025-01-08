74a4a68ca3db1eb2eec8341492a17d13071a8832
milson94 <mangueleedmilson@gmail.com>
2025-01-08 20:33:11 +0200

    Deploy app with tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html", // Add paths to your HTML files
+    "./**/*.html",  // Include all HTML files in your project
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
